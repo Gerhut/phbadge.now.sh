89f98af20ca7f63282e0cb440430dc51c651f9f1
blade254353074 <blade254353074@hotmail.com>
2020-02-24 23:37:54 +0800

    feat: create home page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.div`
+  font-size: 24px;
+  text-align: center;
+  color: #f90;
+`
+
+function HomePage () {
+  return <Title>Welcome to Next.js!</Title>
+}
+
+export default HomePage
